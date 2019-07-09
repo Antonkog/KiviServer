9244081cce27b84ab75f691f32d0814dd5c66390
Anton Kogan <akogan777@gmail.com>
2019-07-09 18:24:13 +0300

    @KIS-143 #VN 1.2.37 realtek 9 lib's fix for keyboard.

--- sign_install_rtk9.sh
+++ sign_install_rtk9.sh
@@ -12,12 +12,11 @@ sign509="/Users/antonio/Documents/Auto-Sign/realtek9/platform.x509.pem"
 autoSign="/Users/antonio/Documents/Auto-Sign/signapk.jar"
 
 apk_host="./app/build/outputs/apk/realtek_9_/debug/app-realtek_9_-debug.apk"
-apk_host2="./app/build/outputs/apk/realtek_9_/debug/server_v2rtk9.apk"
+apk_host2="./app/build/outputs/apk/realtek_9_/debug/kiviserver_9.apk"
+path_sysapp="/system/priv-app/kiviserver_9/kiviserver_9.apk" # assuming the app is priviledged
 
-path_sysapp="/system/priv-app/KiviServer/KiviServer.apk" # assuming the app is priviledged
-
-#./gradlew assembleDebug || exit -1 # exit on failure
-$ADB connect 192.168.0.144:5555 || exit -1
+./gradlew assembleDebug || exit -1 # exit on failure
+$ADB connect 192.168.0.177:5555 || exit -1
 
 java -jar ${autoSign} ${sign509} ${signPk8} ${apk_host}  ${apk_host2}
 
@@ -25,7 +24,7 @@ java -jar ${autoSign} ${sign509} ${signPk8} ${apk_host}  ${apk_host2}
 $ADB devices -l
 $ADB root 2> /dev/null
 
-$ADB connect 192.168.0.144:5555 || exit -1
+$ADB connect 192.168.0.177:5555 || exit -1
 
 
 $ADB remount # mount system
